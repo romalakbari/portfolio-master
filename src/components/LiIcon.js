0d48226209a65fb344eb39b0993b90bed2144822
romal-akbari <121485537+romalakbari@users.noreply.github.com>
2024-12-16 15:05:03 +0430

    Update LiIcon.js

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -11,7 +11,7 @@ const LiIcon = ({reference})=>{
   )
   return (
 
-    <figure className ='absolute left-0 stroke-dark dark:stroke-light'>
+    <figure className ='absolute left-0 md:left-[-13%] sm:left[-13%] stroke-dark dark:stroke-light'>
         <svg className='-rotate-90 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px] ' width ="75" height="75" viewBox='0 0 100 100'>
           <circle cx="75" cy="50" r="20" className ='stroke-primary dark:stroke-primary-dark stroke-1 fill-none'/>
           <motion.circle cx="75" cy="50" r="20" className =' stroke-[5px] fill-light dark:fill-dark'
